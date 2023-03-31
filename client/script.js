7493387691382c27ea9032683aff6262de1bff9a
Kaan <kenkaraa@gmail.com>
2023-03-31 15:31:06 -0400

    Adds handleSubmit

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from "./assets/bog.svg";
+import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
@@ -22,10 +22,11 @@ function typeText(element, text) {
   let index = 0;
 
   let interval = setInterval(() => {
-    if (index < text.length){element.innerHTML += text.chartAt(index);
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
       index++;
     } else {
-      clearInterval(interval)
+      clearInterval(interval);
     }
   }, 20);
 }
@@ -37,3 +38,51 @@ function generateUniqueId() {
 
   return `id-${timestamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+        <div class="chat">
+            <div class="profile">
+                <img 
+                  src=${isAi ? bot : user} 
+                  alt="${isAi ? "bot" : "user"}" 
+                />
+            </div>
+            <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+    </div>
+`;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  // to clear the textarea input
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div
+  const messageDiv = document.getElementById(uniqueId);
+
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
