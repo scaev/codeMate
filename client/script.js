977c345e4c72524f8d9af388a9c1925c91a81296
Kaan <kenkaraa@gmail.com>
2023-03-31 15:00:44 -0400

    Defines loader and typeText functions

---
+++ client/script.js
@@ -0,0 +1,31 @@
+import bot from "./assets/bog.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length){element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval)
+    }
+  }, 20);
+}
